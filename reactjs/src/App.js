7144f695bc3fefc09a1d040530ae56c83107e1c1
Marcelo Ratton <rattones@marceloratton.com>
2019-06-11 01:17:20 -0300

    more classes , not finished yet

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import api from './services/api'
 
-// import './style.css'
+import './style.css'
 
 import Header from './components/Header'
 import Main from './pages/main'
